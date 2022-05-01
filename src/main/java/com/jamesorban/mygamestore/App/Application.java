a3755886bb34452cafc277871839bc8d2066dc75
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-05-01 22:38:46 +0200

    first commit

---
+++ src/main/java/com/jamesorban/mygamestore/App/Application.java
@@ -0,0 +1,33 @@
+package com.jamesorban.mygamestore.App;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import pl.myGamesLibrary.database.dbuitls.DBManager;
+import pl.myGamesLibrary.utils.FillDatabase;
+import pl.myGamesLibrary.utils.FxmlUtils;
+
+
+public class Application extends javafx.application.Application {
+
+    public static final String APPLICATION_FXML = "/fxml/Application.fxml";
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane borderPane = FxmlUtils.fxmlLoader(APPLICATION_FXML);
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(FxmlUtils.getResourceBundle().getString("title.application"));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+        DBManager.initDatabase();
+        FillDatabase.fillDatabase(); //here an additional code is launched that fills the database
+    }
+}
