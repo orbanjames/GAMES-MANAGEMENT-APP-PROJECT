6dcc0ab370dce450891c1b3a2cdfa38a6a4cd217
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-05-05 06:59:52 +0200

    subsequent commits

--- src/main/java/com/jamesorban/mygamestore/controllers/CategoryController.java
+++ src/main/java/com/jamesorban/mygamestore/controllers/CategoryController.java
@@ -1,4 +1,96 @@
 package com.jamesorban.mygamestore.controllers;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import com.jamesorban.mygamestore.modelFx.CategoryFx;
+import com.jamesorban.mygamestore.modelFx.CategoryModel;
+import com.jamesorban.mygamestore.utils.DialogUtils;
+import com.jamesorban.mygamestore.utils.exceptions.ApplicationException;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+
 public class CategoryController {
+
+    @FXML
+    private Button addCategoryButton;
+    @FXML
+    private Button editCategoryButton;
+    @FXML
+    private Button deleteCategoryButton;
+    @FXML
+    private TextField categoryTextField;
+    @FXML
+    private ComboBox<CategoryFx> categoryComboBox;
+    @FXML
+    private TreeView<String> categoryTreeView;
+
+    private CategoryModel categoryModel;
+
+
+    @FXML
+    public void initialize() {
+        this.categoryModel = new CategoryModel();
+        try {
+            this.categoryModel.init();
+        } catch (ApplicationException e) {
+            DialogUtils.errorDialog(e.getMessage());
+        }
+
+        initBindings();
+    }
+
+    private void initBindings() {
+        this.categoryComboBox.setItems(this.categoryModel.getCategoryList());
+        this.categoryTreeView.setRoot(this.categoryModel.getRoot());
+
+        this.addCategoryButton.disableProperty().bind(categoryTextField.textProperty().isEmpty());
+        this.deleteCategoryButton.disableProperty().bind(this.categoryModel.categoryProperty().isNull());
+        this.editCategoryButton.disableProperty().bind(this.categoryModel.categoryProperty().isNull());
+    }
+
+    @FXML
+    public void addCategoryOnAction() {
+        try {
+            categoryModel.saveCategoryInDataBase(categoryTextField.getText());
+        } catch (ApplicationException e) {
+            DialogUtils.errorDialog(e.getMessage());
+        }
+        categoryTextField.clear();
+    }
+
+    @FXML
+    public void onActionComboBox() {
+        this.categoryModel.setCategory(this.categoryComboBox.getSelectionModel().getSelectedItem());
+    }
+
+    @FXML
+    public void onActionDeleteButton() {
+        Optional<ButtonType> result = DialogUtils.confirmationDialog("delete.category.title", "delete.category.header", "delete.category.content");
+        if(result.get() == ButtonType.OK){
+            try {
+                this.categoryModel.deleteCatogoryById();
+            } catch (ApplicationException | SQLException e) {
+                DialogUtils.errorDialog(e.getMessage());
+            }
+        } else{
+            this.categoryComboBox.getSelectionModel().clearSelection();
+        }
+    }
+
+    @FXML
+    public void onActionEditCategory() {
+        String newCategoryName = DialogUtils.editDialog(this.categoryModel.getCategory().getName());
+        if(newCategoryName != null){
+            this.categoryModel.getCategory().setName(newCategoryName);
+            try {
+                this.categoryModel.updateCategoryInDataBase();
+            } catch (ApplicationException e) {
+                DialogUtils.errorDialog(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/com/jamesorban/mygamestore/controllers/GameController.java
+++ src/main/java/com/jamesorban/mygamestore/controllers/GameController.java
@@ -1,4 +1,110 @@
 package com.jamesorban.mygamestore.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import com.jamesorban.mygamestore.modelFx.AuthorFx;
+import com.jamesorban.mygamestore.modelFx.CategoryFx;
+import com.jamesorban.mygamestore.modelFx.GameModel;
+import com.jamesorban.mygamestore.utils.DialogUtils;
+import com.jamesorban.mygamestore.utils.exceptions.ApplicationException;
+
 public class GameController {
+
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private ComboBox<CategoryFx> categoryComboBox;
+    @FXML
+    private ComboBox<AuthorFx> authorComboBox;
+    @FXML
+    private TextArea descTextArea;
+    @FXML
+    private Slider ratingSlider;
+    @FXML
+    private DatePicker releaseDatePicker;
+    @FXML
+    public Button addGameButton;
+    @FXML
+    public Button clearControlsButton;
+
+    private GameModel gameModel;
+
+
+    @FXML
+    public void initialize(){
+        this.gameModel = new GameModel();
+
+        try {
+            this.gameModel.init();
+        } catch (ApplicationException e) {
+            DialogUtils.errorDialog(e.getMessage());
+        }
+
+        bindings();
+        validation();
+    }
+
+    public void bindings() {
+        this.categoryComboBox.setItems(this.gameModel.getCategoryFxObservableList());
+        this.authorComboBox.setItems(this.gameModel.getAuthorFxObservableList());
+
+//        this.gameModel.getGameFxObjectProperty().titleProperty().bind(this.titleTextField.textProperty());
+//        this.gameModel.getGameFxObjectProperty().categoryFxProperty().bind(this.categoryComboBox.valueProperty());
+//        this.gameModel.getGameFxObjectProperty().authorFxProperty().bind(this.authorComboBox.valueProperty());
+//        this.gameModel.getGameFxObjectProperty().descriptionProperty().bind(this.descTextArea.textProperty());
+//        this.gameModel.getGameFxObjectProperty().ratingProperty().bind(this.ratingSlider.valueProperty());
+//        this.gameModel.getGameFxObjectProperty().releaseDateProperty().bind(this.releaseDatePicker.valueProperty());
+
+        this.authorComboBox.valueProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().authorFxProperty());
+        this.categoryComboBox.valueProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().categoryFxProperty());
+        this.titleTextField.textProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().titleProperty());
+        this.descTextArea.textProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().descriptionProperty());
+        this.ratingSlider.valueProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().ratingProperty());
+        this.releaseDatePicker.valueProperty().bindBidirectional(this.gameModel.getGameFxObjectProperty().releaseDateProperty());
+
+    }
+
+    private void validation() {
+        this.addGameButton.disableProperty().bind(this.titleTextField.textProperty().isEmpty()
+                .or(this.categoryComboBox.valueProperty().isNull())
+                .or(this.authorComboBox.valueProperty().isNull())
+                .or(this.descTextArea.textProperty().isEmpty())
+                .or(this.releaseDatePicker.valueProperty().isNull())
+        );
+
+        this.clearControlsButton.disableProperty().bind(titleTextField.textProperty().isEmpty()
+                .and(this.gameModel.getGameFxObjectProperty().categoryFxProperty().isNull())
+                .and(this.gameModel.getGameFxObjectProperty().authorFxProperty().isNull())
+                .and(this.gameModel.getGameFxObjectProperty().descriptionProperty().isEmpty())
+                .and(this.gameModel.getGameFxObjectProperty().releaseDateProperty().isNull()));
+    }
+
+    @FXML
+    public void addGameOnAction() {
+        try {
+            this.gameModel.saveGameInDataBase();
+        } catch (ApplicationException e) {
+            DialogUtils.errorDialog(e.getMessage());
+        }
+
+    }
+
+    @FXML
+    public void clearControlsOnAction() {
+        clearFields();
+    }
+
+    private void clearFields(){
+        this.titleTextField.clear();
+        this.categoryComboBox.getSelectionModel().clearSelection();
+        this.authorComboBox.getSelectionModel().clearSelection();
+        this.descTextArea.clear();
+        this.ratingSlider.setValue(1);
+        this.releaseDatePicker.getEditor().clear();
+    }
+
+    public GameModel getGameModel() {
+        return gameModel;
+    }
 }
