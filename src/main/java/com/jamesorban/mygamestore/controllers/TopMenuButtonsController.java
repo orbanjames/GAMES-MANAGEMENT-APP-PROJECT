d35a656e0dd5d7d882e9c587cabf6e094317b306
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-05-05 07:01:17 +0200

    updates and subsequent commits

---
+++ src/main/java/com/jamesorban/mygamestore/controllers/TopMenuButtonsController.java
@@ -0,0 +1,49 @@
+package com.jamesorban.mygamestore.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+import com.jamesorban.mygamestore.database.models.User;
+
+public class TopMenuButtonsController {
+
+    public static final String MY_GAMES_LIBRARY_FXML = "/fxml/MyGamesLibrary.fxml";
+    public static final String LIST_GAMES_FXML = "/fxml/GamesList.fxml";
+    public static final String ADD_BOOK_FXML = "/fxml/AddGame.fxml";
+    public static final String ADD_CATEGORY_FXML = "/fxml/AddCategory.fxml";
+    public static final String ADD_AUTHOR_FXML = "/fxml/AddAuthor.fxml";
+
+    private MainController mainController;
+
+    @FXML
+    private ToggleGroup toggleButtons;
+
+    @FXML
+    public void openMyGamesLibrary( ) {
+        this.mainController.setCenter(MY_GAMES_LIBRARY_FXML);
+    }
+
+    @FXML
+    public void openListGames( ) {
+        this.mainController.setCenter(LIST_GAMES_FXML);
+    }
+
+    @FXML
+    public void addGame() {
+        this.mainController.setCenter(ADD_BOOK_FXML);
+    }
+
+    @FXML
+    public void addCategory() {
+        this.mainController.setCenter(ADD_CATEGORY_FXML);
+    }
+
+    @FXML
+    public void addAuthor() {
+        this.mainController.setCenter(ADD_AUTHOR_FXML);
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+}
