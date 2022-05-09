60819852967aa311bb7c2dcf17b556e069585dfb
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-05-09 17:01:55 +0200

    subsequent commits and updates

---
+++ src/main/java/com/jamesorban/mygamestore/utils/exceptions/ApplicationException.java
@@ -0,0 +1,8 @@
+package com.jamesorban.mygamestore.utils.exceptions;
+
+public class ApplicationException extends Exception{
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
